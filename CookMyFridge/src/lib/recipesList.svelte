35c61c6fcc37e9e9421378a997f3c8567533735a
Marian Rickert <marianrickert@Marians-MacBook-Pro.local>
2024-04-15 21:58:29 +0200

    alles wieder da

---
+++ CookMyFridge/src/lib/recipesList.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import {goto} from "$app/navigation";
+
+    export let recipes;
+
+    function viewDetails(recipeId: any) {
+        goto(`/MyRecipes/recipe/${recipeId}`);
+    }
+</script>
+
+<div class="flex flex-col justify-center items-center px-6 py-8">
+    <h1 class="text-white text-4xl mb-4">Recipes</h1>
+    {#each recipes as recipe}
+        <div class="bg-secondary-400 w-2/3 py-2 rounded-xl mt-4 ps-5 flex justify-between">
+            <div>
+                <h2 class="text-white text-2xl">{ recipe.title }</h2>
+                <p class="text-white text-lg">
+                    { recipe.time } { recipe.time === 1 ? 'min' : 'mins' } | { recipe.nutritionType }
+                </p>
+                <p class="text-white text-lg">Needed
+                    Additions: { !recipe.additions.length ? 'None' : recipe.additions.join(', ') }
+                </p>
+            </div>
+            <button on:click={() => viewDetails(recipe.id)}
+                    class="bg-primary-500 text-white rounded-xl px-2 me-5">
+                View Details
+            </button>
+        </div>
+    {/each}
+</div>
--- CookMyFridge/src/routes/NewRecipe/+page.svelte
+++ CookMyFridge/src/routes/NewRecipe/+page.svelte
@@ -5,6 +5,9 @@
     let videoElement: HTMLVideoElement | null = null;
     let errorMessage: string | null = null;
 
+    let image;
+    let items = [];
+
     async function startCamera() {
         try {
             stream = await navigator.mediaDevices.getUserMedia({video: true});
@@ -27,7 +30,9 @@
                 context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
             }
             const photoDataUrl = canvas.toDataURL('image/png');
-            uploadPhoto(photoDataUrl);
+            // remove prefix from data URL
+            const photoDataUrlWithoutPrefix = photoDataUrl.split(',')[1];
+            uploadPhoto(photoDataUrlWithoutPrefix);
         }
     }
 
@@ -51,14 +56,19 @@
     async function uploadPhoto(photoDataUrl: string) {
         try {
             // Make a POST request to the API endpoint
-            const response = await fetch('https://example.com/api/upload-photo', {
+            const response = await fetch('http://0.0.0.0:8000/scan', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ photo: photoDataUrl })
+                body: JSON.stringify({image64: photoDataUrl})
             });
-            if (!response.ok) {
+            if (response.ok) {
+                const data = await response.json();
+                // decode base64 image
+                image = "data:image/jpg;base64," + data.image;
+                items = data.items;
+            } else {
                 throw new Error('Failed to upload photo');
             }
             console.log('Photo uploaded successfully');
@@ -78,19 +88,38 @@
     <div class="max-w-2xl mx-auto p-6 bg-secondary rounded-lg shadow-lg text-white">
         <h1 class="text-3xl font-bold mb-6">Take a Photo</h1>
         <div class="relative">
-            <video bind:this={videoElement} autoplay class="w-full rounded-lg shadow-md">
-                <track kind="captions" src="">
-            </video>
-            <button on:click={capturePhoto}
-                    class="absolute bottom-0 right-0 m-4 bg-blue-500 text-white px-4 py-2 rounded-lg shadow-md">Capture
-            </button>
-            {#if errorMessage}
-                <p class="absolute bottom-0 left-0 m-4 text-red-500">{errorMessage}</p>
+            {#if items.length}
+                {#await image}
+                    <p>Loading...</p>
+                {:then image}
+                    {#if image}
+                        <img src={image} class="w-full rounded-lg shadow-md" alt="">
+                    {/if}
+                {/await}
+            {:else}
+                <video bind:this={videoElement} autoplay class="w-full rounded-lg shadow-md">
+                    <track kind="captions" src="">
+                </video>
+                <button on:click={capturePhoto}
+                        class="absolute bottom-0 right-0 m-4 bg-blue-500 text-white px-4 py-2 rounded-lg shadow-md">
+                    Capture
+                </button>
+                {#if errorMessage}
+                    <p class="absolute bottom-0 left-0 m-4 text-red-500">{errorMessage}</p>
+                {/if}
             {/if}
         </div>
         <div class="mt-4">
             <input type="file" accept="image/*" on:change={handleFileUpload}>
         </div>
+        {#if items.length > 0}
+            <div class="mt-4">
+                <h2 class="text-2xl font-bold mb-4">Items:</h2>
+                {#each items as item (item.name)}
+                    <p>{item.name}: {item.count}</p>
+                {/each}
+            </div>
+        {/if}
     </div>
 </div>
 

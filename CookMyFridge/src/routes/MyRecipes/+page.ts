64e5197471ee04df39065c5849cd7085fa8d8d96
Marian Rickert <marianrickert@Marians-MacBook-Pro.local>
2024-04-16 18:33:49 +0200

    API and adjustments for my recipes

---
+++ CookMyFridge/src/routes/MyRecipes/+page.ts
@@ -0,0 +1,19 @@
+export const load = async ({fetch}) => {
+    const fetchRecipes = async () => {
+        try {
+            const response = await fetch('http://0.0.0.0:8000/recipes', {method: 'GET'});
+            if (response.ok) {
+                const data = await response.json();
+                return {status: 200, recipes: await data};
+            } else {
+                return {status: 500, message: 'Failed to fetch recipes'};
+            }
+        } catch (error) {
+            return {status: 500, message: 'Failed to fetch recipes: ' + error};
+        }
+    };
+
+    return {
+        recipes: await fetchRecipes()
+    };
+};
